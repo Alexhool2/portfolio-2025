cafddc2c6250725d99b1e28498871a6a2ea156fe
Alexhool2 <alederbal@hotmail.com>
2025-05-20 09:17:04 +0900

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { HashRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import PageNotFound from "./pages/PageNotFound.jsx"
-import { Toaster } from "./components/ui/toaster.jsx"
 function App() {
   return (
-    <>
-      <Toaster />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </HashRouter>
-    </>
+    <BrowserRouter basename="/portfolio-2025">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
